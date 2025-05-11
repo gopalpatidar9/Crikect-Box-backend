f9a163272a9df90b184910055619ee11f63394d5
gopalpatidar9 <gopalpatidara9@gmail.com>
2025-05-11 18:18:56 +0530

    update organization model code

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,17 +1,19 @@
 class Organization < ApplicationRecord
     after_create :create_tenant
-
+  
     def create_tenant
       schema_name = self.database.presence || name.parameterize.underscore
-    
-      # Set and persist the sanitized schema name
       self.update_column(:database, schema_name) if self.database.blank?
-    
-      # Avoid creation if schema already exists
-      unless Apartment.tenant_names.include?(schema_name)
-        Apartment::Tenant.create(schema_name)
-      rescue Apartment::TenantExists
-        Rails.logger.warn "Tenant #{schema_name} already exists"
+  
+      begin
+        unless Apartment.tenant_names.include?(schema_name)
+          Apartment::Tenant.create(schema_name)
+        else
+          Rails.logger.info "Tenant #{schema_name} already exists"
+        end
+      rescue Apartment::TenantExists => e
+        Rails.logger.warn "Tenant creation failed for #{schema_name}: #{e.message}"
       end
-    end      
+    end
 end
+  
