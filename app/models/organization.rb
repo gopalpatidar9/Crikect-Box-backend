13d7a5699c52ce4f26611e864b93aec72938a937
gopalpatidar9 <gopalpatidara9@gmail.com>
2025-05-11 13:58:14 +0530

    update apartment method in organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,10 @@
 class Organization < ApplicationRecord
   after_create do
-    schema_name = self.name.parameterize.underscore
-    update_column(:database, schema_name)
-    Apartment::Tenant.create(schema_name)
-  end      
+    schema = self.name.parameterize.underscore
+    self.update_column(:database, schema)
+      
+    unless Apartment.tenant_names.include?(schema)
+      Apartment::Tenant.create(schema)
+    end
+  end       
 end
