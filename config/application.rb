f0ecf2c6c930375840fb837111295f40fce79c71
gopalpatidar9 <gopalpatidara9@gmail.com>
2025-05-12 10:08:45 +0530

    Update tenant

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
+
 require_relative "boot"
 
 require "rails/all"
+Dotenv::Railtie.load if defined?(Dotenv)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,16 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: 587, 
+    user_name: ENV['GMAIL_USERNAME'], 
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
