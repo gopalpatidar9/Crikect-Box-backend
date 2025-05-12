845f7dc825e5f2c16d9ebf260086bbf326ed1981
gopalpatidar9 <gopalpatidara9@gmail.com>
2025-05-12 11:48:51 +0530

    chnage tenatnt to public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :switch_to_public_schema
+
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::MimeResponds
   include ActionController::Serialization
@@ -9,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :mobilephone, :external_id, :last_login, :is_active ])
+  end
+
+  def switch_to_public_schema
+    ActiveRecord::Base.connection.schema_search_path = 'public'
   end
 end
